054a77b82b4849e9ddad82ab841e69f4ca5fea7c
Alina0708 <alina.sevryuk0708@gmail.com>
2021-08-04 18:16:10 +0300

    add logic who is winner

--- projects/project3.js
+++ projects/project3.js
@@ -10,15 +10,22 @@ let game = (event) => {
       }
     }
     hod++;
+
+    if (hod == 9) {
+      document.getElementById("words").innerHTML = "Ничья";
+    }
+
     checkWinner();
   }
 
 }
-let currentState = []
+
+
+
 const checkWinner = () => {
-  const allcell = Array.prototype.slice.call(document.getElementsByClassName("cell")).map((item) =>  item.innerHTML);
+  let allcell = Array.prototype.slice.call(document.getElementsByClassName("cell")).map((item) => item.innerHTML);
   console.log('allcell', allcell)
-  
+
   const arr = [
     [0, 1, 2],
     [3, 4, 5],
@@ -30,17 +37,38 @@ const checkWinner = () => {
     [2, 4, 6]
   ];
 
-  
+  let numberWinner0 = 0, numberWinnerX = 0;
   for (let i = 0; i < arr.length; i++) {
     let wc = arr[i];
     if (allcell[wc[0]] == allcell[wc[1]] && allcell[wc[1]] == allcell[wc[2]] && allcell[wc[0]] != "") {
-      console.log('win')
-      // alert("xnjn");
-      // location.reload();
-    }
 
+      let winner = allcell[wc[2]];
+      // location.reload();
+      if (winner == "X") {
+        numberWinnerX++;
+        console.log(' numberWinnerX++;' + numberWinnerX);
+        document.getElementById("words").innerHTML = "Победил игрок " + winner;
+      }
+
+
+      if (winner == "0") {
+        numberWinner0++;
+        document.getElementById("words").innerHTML = "Победил игрок " + winner;
+      }
+
+    }
   }
+
+
 
 
 }
 
+
+const cleaning = () => {
+  let clean = document.getElementsByClassName("cell");
+  for (let i = 0; i < 9; i++) {
+    clean[i].innerHTML = "";
+  }
+  hod = 0;
+}
